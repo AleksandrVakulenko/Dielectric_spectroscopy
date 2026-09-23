1e69298056cea43e7462d1ed61c363a94f437996
Aleksandr Vakulenko <sasha705@mail.ru>
2026-09-24 02:23:21 +0300

    add test for LCR device

---
+++ TEST_LCR_only.m
@@ -0,0 +1,52 @@
+
+
+clc
+
+
+LCR_dev_class_name = "LCR_8230_dev";
+
+
+Time_profile = "fine"; % "ultra_fast", "common", "fine", "most_accurate"
+
+Gen_Voltage_level = 1; % [V]
+F_min = 10; % [Hz]
+F_max = 1e6;
+F_num = 50;
+
+
+Freq_arr = freq_gen.td_fra(F_min, F_max, F_num);
+
+
+
+Settings.gen_amp = Gen_Voltage_level;
+Settings.time_profile = Time_profile;
+Settings.freq_list = Freq_arr;
+Settings.dc_bias = 0; % NOTE: do not use
+Settings.harm_num = 1; % NOTE: do not use
+Settings.noise_env_flag = false; % NOTE: do not use
+
+
+Fig_FRA = init_FRA_figure();
+Result = Measure_LCR(LCR_dev_class_name, Settings);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
